df08696522780c6d505015353838a486152e9c33
Max Maliuk <ashlion.timeless@gmail.com>
2019-05-31 12:19:33 +0300

    Added DataPurchaseContract

---
+++ lib/contract/DataPurchaseContract.sol
@@ -0,0 +1,213 @@
+pragma solidity 0.4.24;
+
+contract Ownable {
+    address public owner;
+
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+}
+
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+    /**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+            return 0;
+        }
+
+        c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+/**
+ * @title ERC20Basic
+ * @dev Simpler version of ERC20 interface
+ * See https://github.com/ethereum/EIPs/issues/179
+ */
+contract ERC20Basic {
+    function totalSupply() public view returns (uint256);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+/**
+ * @title Basic token
+ * @dev Basic version of StandardToken, with no allowances.
+ */
+contract BasicToken is ERC20Basic {
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+
+    uint256 totalSupply_;
+
+    /**
+    * @dev Total number of tokens in existence
+    */
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    /**
+    * @dev Transfer token for a specified address
+    * @param _to The address to transfer to.
+    * @param _value The amount to be transferred.
+    */
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "Address must not be zero.");
+        require(_value <= balances[msg.sender], "There is no enough balance.");
+
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+    * @dev Gets the balance of the specified address.
+    * @param _owner The address to query the the balance of.
+    * @return An uint256 representing the amount owned by the passed address.
+    */
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
+
+}
+
+/**
+ * @title ERC20 interface
+ * @dev see https://github.com/ethereum/EIPs/issues/20
+ */
+contract ERC20 is ERC20Basic {
+    function allowance(address owner, address spender)
+        public view returns (uint256);
+
+    function transferFrom(address from, address to, uint256 value)
+        public returns (bool);
+
+    function approve(address spender, uint256 value) public returns (bool);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+contract PROMDataMarket is Ownable {
+
+    using SafeMath for uint256;
+    
+    address token;
+    
+    event DataOwnerPurchased(address buyer, address _owner, uint256 _value);
+
+    constructor(address _token) public {
+        token = _token;
+    }
+    
+    /**
+    * @dev Receive payment in tokens and distribute it between data owner, data validator and the system.
+    * @param _owner The address which owns the data.
+    * @param _validator The address which has validated the data.
+    * @param _value The amount of tokens to be paid for the data.
+    */
+    function purchaseDataOwner(address _owner, address _validator, uint256 _value) public {
+        require(_value>0);
+        ERC20 erc20 = ERC20(token);
+        require(erc20.allowance(msg.sender,this)>_value,"You have to allow DataMarket contract to transfer tokens from your account");
+        emit DataOwnerPurchased(msg.sender,_owner,_value);
+        erc20.transferFrom(msg.sender,this,_value.div(100).mul(15));
+        erc20.transferFrom(msg.sender,_owner,_value.div(100).mul(50));
+        erc20.transferFrom(msg.sender,_validator,_value.div(100).mul(25));
+    }
+
+    /**
+    * @dev Withdraw tokens from this contract's token balance. Usable only by this contract's owner.
+    */
+    function withdrawTokens() public onlyOwner {
+        ERC20(token).transfer(owner,ERC20(token).balanceOf(this));
+    }
+
+    /**
+    * @dev Set ERC20 token which will be used to conduct payments through this contract. Usable only by this contract's owner.
+    * @param _token The address of the ERC20 token contract.
+    */
+    function setToken(address _token) public onlyOwner {
+        token = _token;
+    }
+
+    /**
+    * @dev Get the address of ERC20 token which is used to conduct payments through this contract.
+    */
+    function getToken() public view returns(address){
+        return token;
+    }
+}
